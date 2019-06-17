9aafa263e1d8eea5a446fa286c346c313ee78432
simeon9696 <31748055+simeon9696@users.noreply.github.com>
2019-06-17 04:11:24 -0400

    Delete registrationsuccess.js

--- EmployTT/scripts/registrationsuccess.js
+++
@@ -1,6 +0,0 @@
-const jobBtn = document.querySelector("#jobButton");
-
-jobBtn.addEventListener('click',(btnPressListener)=>{
-    btnPressListener.preventDefault();
-    window.location.href = 'index.html';
-})
