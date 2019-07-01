b46512c999e5c06f2d13056d60714d8e19f97e7f
Sasha Singh <sasha.j.singh@gmail.com>
2019-07-01 18:23:18 -0400

    Update jobdescription.js

--- EmployTT/scripts/jobdescription.js
+++ EmployTT/scripts/jobdescription.js
@@ -28,7 +28,7 @@
         if (user.admin){
           let display = document.querySelector('#username');
           userName.style.display = "block";
-          userName.innerHTML = '<img src="../images/webpImages/user-icon.webp" onerror="this.onerror=null; this.src=\'./images/user-icon.png\'" width="13" height="auto">&nbsp;'+user.displayName; 
+          userName.innerHTML = '<img src="../images/webpImages/user-icon.webp" onerror="this.onerror=null; this.src=\'../images/user-icon.png\'" width="13" height="auto">&nbsp;'+user.displayName; 
     
           let logOut= document.querySelector("#logged-in");
           logOut.innerHTML = "Log Out";
@@ -59,7 +59,7 @@
           console.log("I am an MDA");
           let display = document.querySelector('#username');
           userName.style.display = "block";
-          userName.innerHTML = '<img src="../images/webpImages/user-icon.webp" onerror="this.onerror=null; this.src=\'./images/user-icon.png\'" width="13" height="auto">&nbsp;'+user.displayName; 
+          userName.innerHTML = '<img src="../images/webpImages/user-icon.webp" onerror="this.onerror=null; this.src=\'../images/user-icon.png\'" width="13" height="auto">&nbsp;'+user.displayName; 
     
           let logOut= document.querySelector("#logged-in");
           logOut.innerHTML = "Log Out";
@@ -81,7 +81,7 @@
           console.log("I am a civilian");
           let display = document.querySelector('#username');
           userName.style.display = "block";
-          userName.innerHTML = '<img src="../images/webpImages/user-icon.webp" onerror="this.onerror=null; this.src=\'./images/user-icon.png\'" width="13" height="auto">&nbsp;'+user.displayName; 
+          userName.innerHTML = '<img src="../images/webpImages/user-icon.webp" onerror="this.onerror=null; this.src=\'../images/user-icon.png\'" width="13" height="auto">&nbsp;'+user.displayName; 
     
           let logOut= document.querySelector("#logged-in");
           logOut.innerHTML = "Log Out";
