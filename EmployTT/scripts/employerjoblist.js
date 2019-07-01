ccc32fdfb13ca2eb1d8f93074a741c7ef3dc86da
Sasha Singh <sasha.j.singh@gmail.com>
2019-07-01 18:21:21 -0400

    Update employerjoblist.js

--- EmployTT/scripts/employerjoblist.js
+++ EmployTT/scripts/employerjoblist.js
@@ -26,7 +26,7 @@ auth.onAuthStateChanged(user => {
       if (user.admin){
         let display = document.querySelector('#username');
         userName.style.display = "block";
-        userName.innerHTML = '<img src="../images/webpImages/user-icon.webp" onerror="this.onerror=null; this.src=\'./images/user-icon.png\'" width="13" height="auto">&nbsp;'+user.displayName; 
+        userName.innerHTML = '<img src="../images/webpImages/user-icon.webp" onerror="this.onerror=null; this.src=\'../images/user-icon.png\'" width="13" height="auto">&nbsp;'+user.displayName; 
   
         let logOut= document.querySelector("#logged-in");
         logOut.innerHTML = "Log Out";
