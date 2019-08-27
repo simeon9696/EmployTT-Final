abad9de5cc0b49c4a9e8c37e89035f04431dc862
wetcrix <32115812+wetcrix@users.noreply.github.com>
2019-08-27 11:49:32 -0400

    Removed tables and added query for joblist

--- EmployTT/index.js
+++ EmployTT/index.js
@@ -338,49 +338,6 @@ function fadeInRegistrationForm(){
 
 
 /////////////////////////add above/////////////////////////
-const jobTable = document.querySelector('#job-table');
-let c = 0;
-let table = document.createElement('table');
-let body = document.createElement('tbody');
-const div = document.querySelector('#job-table');
-var tabs = document.querySelectorAll(".tabCont .butCont button");
-var content = document.querySelectorAll(".tabCont .content");
-
-function renderJobTable(doc, c){
-  let node = document.createElement('tr');
-  let employer = document.createElement('td');
-  let category= document.createElement('td');
-  let required = document.createElement('td');
-  let status = document.createElement('td');
-  let deadline = document.createElement('td');
-  let about = document.createElement('td');
-  let applyButton = document.createElement('button');
-  applyButton.setAttribute("id",c);
-  var jobid = doc.id;
-  applyButton.setAttribute("name", jobid);
-  applyButton.setAttribute("onClick","clickedButton(this.id, this.name)");
-
-  node.setAttribute('doc-id',doc.id);
-  employer.textContent = doc.data().employer;
-  category.textContent = doc.data().category;
-  required.textContent = doc.data().skills;
-  status.textContent = doc.data().jobstatus;
-  deadline.textContent = doc.data().deadline;
-  about.textContent = doc.data().about;
-  var texts = document.createTextNode("Apply");
-  applyButton.appendChild(texts);
-
-  node.append(category);
-  node.append(employer);
-  node.append(required);  
-  node.append(status);
-  node.append(deadline);
-  node.append(about);
-  node.append(applyButton);
-  body.append(node);
-  table.appendChild(body);
-  div.append(table);
-}
 
 function clickedButton(id, jobid){
 
@@ -411,83 +368,28 @@ function clickedButton(id, jobid){
   });
 }
 
-function searchDatabase(searchBar, locationBar, categoryBar){
-  firestore.collection('Jobs').get().then((snapshot)=>{
-    var c = 0;
-    snapshot.docs.forEach(doc =>{
-        var about = doc.data().about;
-        searchBar.toLowerCase();
-        about.toLowerCase();
-        c++;
-        var t = about.search(searchBar);
 
-        var location = doc.data().location;
-        locationBar.toLowerCase();
-        location.toLowerCase();
-        var u = location.search(locationBar);
-        
-        var category = doc.data().category;
-        locationBar.toLowerCase();
-        location.toLowerCase();
-        var v = category.search(categoryBar);
-        if(t >= 0 && u >= 0 && v >= 0){
-          renderJobTable(doc);
-        }
-    });
-  });
-}
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~EVENT LISTENER FOR FIND JOB BUTTON~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const findJobs = document.querySelector('#findJobs');
-
-function makeTable(){
-
-  let node = document.createElement('tr');
-  let employer = document.createElement('td');
-  let category= document.createElement('td');
-  let required = document.createElement('td');
-  let status = document.createElement('td');
-  let deadline = document.createElement('td');
-  let about = document.createElement('td');
-  let apply = document.createElement('td');
-
-  employer.textContent = "Employer";
-  category.textContent = "Category";
-  required.textContent = "Required Skills";
-  status.textContent = "Jobstatus";
-  deadline.textContent = "Deadline";
-  about.textContent = "About";
-  apply.textContent = "Press to Apply";
-  
-  node.append(category);
-  node.append(employer);
-  node.append(required);  
-  node.append(status);
-  node.append(deadline);
-  node.append(about);
-  node.append(apply);
-  body.appendChild(node);
-  table.appendChild(body);
-  div.appendChild(table);
-  c = c +1;
-}
-
 
 findJobs.addEventListener('click', (ee)=>{
   ee.preventDefault();
-    if(c>0){
-      var row = 0;
-      while(row = table.rows.length > 0){
-        table.deleteRow(0);
-      }
-
-    }
-    table.setAttribute("class", "jobtable");
-    table.setAttribute("id", "job-table");
-    makeTable();
-  let searchBar = document.getElementById('searchBar').value;
-  let locationBar = document.getElementById('location-filter').value;
-  let categoryBar = document.getElementById('category-filter').value;
-  searchDatabase(searchBar, locationBar, categoryBar);
+  var searchBar, locationBar, categoryBar;
+  searchBar = ""; locationBar = ""; categoryBar = ""; params = "";
+  
+  if(document.getElementById('searchBar').value){
+    searchBar += document.getElementById('searchBar').value;
+  }if(document.getElementById('location-filter').value){
+    locationBar += document.getElementById('location-filter').value;
+  }if(document.getElementById('category-filter').value){
+    categoryBar += document.getElementById('category-filter').value;
+  }
+   var fullPath = window.location.pathname;
+  var path = fullPath.substring(0,fullPath.lastIndexOf("/"));
+  var queryString = "search=" + searchBar + "&location=" + locationBar + "&category=" + categoryBar;
+  var link = window.location.origin + path + "/pages/joblist.html?" + encodeURI(queryString);
+  window.open(link,"_self");
 });
 
 
